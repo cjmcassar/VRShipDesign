431c70250d663ff2712e440d91da48d05037d626
cjmcassarucl <christopher-john.cassar.16@ucl.ac.uk>
2019-02-08 14:21:01 +0000

    - Fixed the update method bug. Used OnEnable instead. The tool works like a charm

--- Assets/Scripts/AttachMaterial.cs
+++ Assets/Scripts/AttachMaterial.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class AttachMaterial : MonoBehaviour
 {
     public GameObject myObject;
@@ -9,29 +10,40 @@ public class AttachMaterial : MonoBehaviour
     public Material PlaneCutter;
 
 
-
     // Update is called once per frame
-    void Update()
+    void OnEnable()
     {
         AttachPlaneCutter();
-    }
 
+    }
 
     //Figure out a way to attach the material to the object
 
     private void AttachPlaneCutter()
     {
-        myObject = GameObject.Find("Layer_0");
-        plane = GameObject.Find("Quad"); // finds the plane object in the scene and attaches it to the script
-        if (myObject.name == "Layer_0" && myObject.GetComponent<OnePlaneCuttingControllerVR>())
-        {
-            ////Resources.Load("Materials/OnePlaneCrossSection", typeof(Material)) as Material;
-            //myObject.GetComponent<Renderer>().material = PlaneCutter;                               // TODO get the material from the plane
-        }
-        else if (myObject.name != "Layer_0" && !myObject.GetComponent<OnePlaneCuttingControllerVR>())
-        {
-            return;
-        }
+
+        myObject = GameObject.Find("MyObject/Layer_0/Layer_0");
+        plane = GameObject.Find("Quad");                                                            // finds the plane object in the scene and attaches it to the script
+        PlaneCutter = Resources.Load("OnePlaneCrossSection", typeof(Material)) as Material;         // finds the material in the resources folder and attaches it to the script
+        myObject.GetComponentInChildren<Renderer>().sharedMaterial = PlaneCutter;                   // Gets the material and attaches it to the obejct
+
+
+        //TODO there is a bug that seems to be asscociated with the update method... Need to find a way to activate this method ONCE...
+
+        #region OldCode
+        //if (myObject.name == "Layer_0" /*&& myObject.GetComponent<OnePlaneCuttingControllerVR>()*/)
+        //{
+
+        //    // TODO get the material from the plane
+        //}
+        //else if (myObject.name != "Layer_0" /*&& !myObject.GetComponent<OnePlaneCuttingControllerVR>()*/)
+        //{
+        //    return;
+        //}
+        #endregion
+
+
     }
+
 
 }
