e2fe77ef22c13710f339e1b5035359627ed5fdc3
cjmcassarucl <christopher-john.cassar.16@ucl.ac.uk>
2019-03-06 17:20:58 +0000

    - Added the find method and instantiate method. - added some TODOs

--- Assets/Scripts/QuadLocation.cs
+++ Assets/Scripts/QuadLocation.cs
@@ -5,28 +5,38 @@ using UnityEngine;
 public class QuadLocation : MonoBehaviour {
 
     public GameObject quad;
+    public GameObject quadClone;
+    public Transform ObjectToHide;
 
 	// Use this for initialization
 	public void Start ()
     {
+        LoadQuad();
+
+    }
+
+    private void LoadQuad()
+    {
         quad = Resources.Load("Quad", typeof(GameObject)) as GameObject;
         GameObject.Instantiate(quad);
+        quadClone = GameObject.Find("Quad(Clone)");
+        ObjectToHide = this.transform;
+        quadClone.transform.SetParent(ObjectToHide);
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update ()
     {
 		
 	}
 
-    private void GetQuad()
+    private void ChangeQuadLocation()
     {
         MeshRenderer renderer = GetComponent<MeshRenderer>();
 
             if (renderer == null )
             {
 
-                //radialmenu.transform.SetParent(renderer[i].transform);
             }
 
             else if (renderer != null)
@@ -36,5 +46,7 @@ public class QuadLocation : MonoBehaviour {
 
     }
 
-    // TODO needs to change the position of the quad to that of the controller
+    // TODO 1 needs to change the position of the quad to that of the controller
+    // TODO 2 the quad could use the transform position of the controller.. 
+    // TODO 3 the issue would be finding the controller. Maybe another gameobject.find would work .
 }
