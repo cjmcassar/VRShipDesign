1b329d69ee86dd79d166cede1882650a9bd2b509
Christopher-John Cassar <cjmcassar@users.noreply.github.com>
2019-05-23 15:38:05 +0100

    - Moved the item screen in front of the player platform. - Fixed a minor bug in the hideshowmenu script. - Created a setactive function for the distance measurer (ActivateDeactivateDM script).

--- Assets/Scripts/ActivateDeactivateDM.cs
+++ Assets/Scripts/ActivateDeactivateDM.cs
@@ -2,15 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HideShowMenu : MonoBehaviour {
+public class ActivateDeactivateDM : MonoBehaviour {
 
     public new GameObject gameObject;
-	// Update is called once per frame
-	public void ToggleMenuOn()
+    // Update is called once per frame
+    public void ToggleDMOn()
     {
         gameObject.SetActive(true);
     }
-    public void ToggleMenuOff()
+    public void ToggleDMOff()
     {
         gameObject.SetActive(false);
     }
