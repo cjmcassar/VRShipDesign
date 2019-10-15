fc3c9620f1ee927e6842638f6cda9e5e068d4f07
Christopher-John Cassar <cjmcassar@users.noreply.github.com>
2019-10-15 17:35:49 +1030

    - Added ColorManager, DrawLineManager, and MeshLineRenderer script to the project. This allows you to draw in VR. - Managed the files and organised the new files into their appropriate folders.

---
+++ Assets/Scripts/Scripts/DrawingFeature/ColorManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorManager : MonoBehaviour {
+
+    public static ColorManager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+    }
+
+    private void OnDestroy()
+    {
+        if (Instance == this)
+            Instance = null;
+    }
+
+    private Color color;
+
+    void OnColorChange(HSBColor color)
+    {
+        this.color = color.ToColor();
+    }
+
+    public Color GetCurrentColor()
+    {
+        return this.color;
+    }
+
+
+}
---
+++ Assets/Scripts/Scripts/DrawingFeature/DrawLineManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawLineManager : MonoBehaviour
+{
+
+    public Material lMat;
+
+    public SteamVR_TrackedObject trackedObj;
+
+    private MeshLineRenderer currLine;
+
+    private int numClicks = 0;
+
+    // Update is called once per frame
+    void Update ()
+    {
+        SteamVR_Controller.Device device = SteamVR_Controller.Input((int)trackedObj.index);
+
+        if (device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            //Debug.LogError("Got Here");
+            GameObject go = new GameObject();
+            go.AddComponent<MeshFilter>();
+            go.AddComponent<MeshRenderer>();
+            currLine = go.AddComponent<MeshLineRenderer>();
+
+            currLine.lmat = new Material (lMat);
+            currLine.setWidth (.1f);
+
+            numClicks = 0;
+        }
+
+        else if (device.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            //#pragma warning disable CS0618 // Type or member is obsolete
+            //            currLine.SetVertexCount(numClicks + 1);
+            //#pragma warning restore CS0618 // Type or member is obsolete
+
+            //            currLine.SetPosition(numClicks, trackedObj.transform.position);
+
+            currLine.AddPoint(trackedObj.transform.position);
+            numClicks++;
+        }
+
+        else if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            numClicks = 0;
+            currLine = null;
+        }
+
+
+            if (currLine != null)
+        {
+            currLine.lmat.color = ColorManager.Instance.GetCurrentColor();
+        }
+    }
+}
