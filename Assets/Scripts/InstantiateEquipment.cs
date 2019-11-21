7cce88eac85acc37e824ebd4fa1341ff59f5e6d8
Christopher-John Cassar <cjmcassar@users.noreply.github.com>
2019-11-21 18:08:12 +1030

    - Added the Equipment objects to the library - Added the equipment to the instantiate script

--- Assets/Scripts/InstantiateEquipment.cs
+++ Assets/Scripts/InstantiateEquipment.cs
@@ -11,15 +11,71 @@ public class InstantiateEquipment : MonoBehaviour {
 /// This uploads the equipment objects from the resources folder.
 /// This code is essentially repeated for each equipment slot.
 /// </ Upload Equipment summary>
+/// 
     public GameObject INS;
     public GameObject INSClone;
+
+    public GameObject Stairwell;
+    public GameObject StairwellClone;
+
+    public GameObject Chair;
+    public GameObject ChairClone;
+
+    public GameObject Toolbox;
+    public GameObject ToolboxClone;
+
+    public GameObject Engine;
+    public GameObject EngineClone;
+
+
+
     public void UploadINS()
     {
-        INS = Resources.Load("INS (Base)", typeof(GameObject)) as GameObject;
+        INS = Resources.Load("INS", typeof(GameObject)) as GameObject;
         GameObject.Instantiate(INS);
-        INSClone = GameObject.Find("INS (Base)(Clone)");
+        INSClone = GameObject.Find("INS (Clone)");
 
         controller = GameObject.Find("Controller (left)");
         INSClone.transform.position = controller.transform.position;
+    }
+
+    public void UploadStairwell()
+    {
+        Stairwell = Resources.Load("Stairwell", typeof(GameObject)) as GameObject;
+        GameObject.Instantiate(Stairwell);
+        Stairwell = GameObject.Find("Stairwell (Clone)");
+
+        controller = GameObject.Find("Controller (left)");
+        StairwellClone.transform.position = controller.transform.position;
+    }
+
+    public void UploadChair()
+    {
+        Chair = Resources.Load("Chair", typeof(GameObject)) as GameObject;
+        GameObject.Instantiate(Chair);
+        Chair = GameObject.Find("Chair (Clone)");
+
+        controller = GameObject.Find("Controller (left)");
+        ChairClone.transform.position = controller.transform.position;
+    }
+
+    public void UploadToolBox()
+    {
+        Toolbox = Resources.Load("Toolbox", typeof(GameObject)) as GameObject;
+        GameObject.Instantiate(Toolbox);
+        Toolbox = GameObject.Find("Toolbox (Clone)");
+
+        controller = GameObject.Find("Controller (left)");
+        ToolboxClone.transform.position = controller.transform.position;
+    }
+
+    public void UploadEngine()
+    {
+        Engine = Resources.Load("Engine", typeof(GameObject)) as GameObject;
+        GameObject.Instantiate(Engine);
+        Engine = GameObject.Find("Engine (Clone)");
+
+        controller = GameObject.Find("Controller (left)");
+        EngineClone.transform.position = controller.transform.position;
     }
 }
